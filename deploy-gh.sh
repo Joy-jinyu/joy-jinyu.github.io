5ef0ae98886253761d4c8be6354a064038873fac
王艺锦 <wangyijin@starlinke.com>
2021-10-28 21:50:37 +0800

    提交代码

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -1,3 +1,8 @@
+git add .
+git commit -m '提交代码'
+# 提交代码
+git push -u origin master
+
 # 确保脚本抛出遇到的错误
 set -e
 
@@ -12,7 +17,7 @@ echo 'joy.github.io' > CNAME
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m '更新文档'
 
 # 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
 git push -f https://github.com/Joy-jinyu/joy.github.io.git master:gh-page
