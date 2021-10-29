ee3d9e093921de27218f5ce955dd5a2a4f7918a3
王艺锦 <wangyijin@starlinke.com>
2021-10-29 17:55:39 +0800

    提交代码

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,12 +8,24 @@ module.exports = {
                 collapsable: true,
                 children: [
                     {
-                        title: "前端",
+                        title: "编码基础",
                         collapsable: true,
                         children: [{
                             title: "开发规范", path: "/fronted/codeBasic/developRule"
                         }]
-                    }],
+                    },
+                    {
+                        title: "项目构建",
+                        collapsable: true,
+                        children: [ {
+                            title: "webpack",
+                            collapsable: true,
+                            children: [{
+                                title: "vue-project", path: "/fronted/codeBasic/webpack/vue-project"
+                            }]
+                        }]
+                    }
+                ],
             },
         ]
     }
